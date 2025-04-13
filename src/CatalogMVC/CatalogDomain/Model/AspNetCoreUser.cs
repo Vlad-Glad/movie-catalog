60beb1e2b116b3d2998daab67599bbc20a99be8f
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2025-04-13 17:29:53 +0300

    Initial commit

---
+++ src/CatalogMVC/CatalogDomain/Model/AspNetCoreUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CatalogDomain.Model
+{
+   public class AspNetCoreUser: IdentityUser
+   {
+        public virtual ICollection<ToWatchList> ToWatchLists { get; set; } = new List<ToWatchList>();
+        public virtual ICollection<UserRating> UserRatings { get; set; } = new List<UserRating>();
+        public virtual ICollection<WatchedList> WatchedLists { get; set; } = new List<WatchedList>();
+   }
+}
+
--- src/CatalogMVC/CatalogDomain/Model/User.cs
+++ src/CatalogMVC/CatalogDomain/Model/User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace CatalogDomain.Model;
 
