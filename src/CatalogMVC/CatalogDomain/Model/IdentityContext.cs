68278a89cd582b85d9b0c9a5298ac852d7ba661b
Владислав Гладкий <vlad.v.gladkiy@gmail.com>
2025-04-13 17:39:45 +0300

    add Microsoft.AspNetCore.Identity.EntityFrameworkCore, create new AspNetCoreUser, new context

---
+++ src/CatalogMVC/CatalogDomain/Model/IdentityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogDomain.Model
+{
+    public class IdentityContext : IdentityDbContext<AspNetCoreUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+    : base(options)
+        {
+        }
+    }
+}
